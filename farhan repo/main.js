84b44f2b9193904827df432cc16458a322fa7ff4
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-03 21:12:14 +0300

    some js calculation

--- farhan repo/main.js
+++ farhan repo/main.js
@@ -1,1 +1,14 @@
 console.log(5);
+let num1 = 6;
+let num2 = 9;
+let num3 = 38;
+
+
+let sum = (num1 + num2 + num3)
+console.log(sum)
+
+let product = (num1 * num2 * num3)
+console.log(product)
+
+let quetient = (num1 / num2 / num3)
+console.log(quetient)
