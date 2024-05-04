88076dd5221f16b176747bbc64fa513cf64c4a7e
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-04 22:15:24 +0300

    doing more on strings and operators

---
+++ moha repo/js-folder/logical.js
@@ -0,0 +1,45 @@
+// they are 3 logical operators && || ! 
+
+// and && 
+
+// let y = 7 
+// let b = 8
+
+// console.log(b > !y);
+// console.log(!b < y);
+
+// console.log()
+
+// Logical AND (&&):Example 1: Checking if both conditions are true before executing code:
+
+// let age = 16;
+// let hasLicense = true;
+
+// if (age >= 18 && hasLicense) {
+//     console.log("You are eligible to drive.");
+// } else {
+//     console.log("You are not eligible to drive.");
+// }
+
+// Logical OR (||):Example 1: Providing multiple conditions for executing code:
+
+let day = "Saturday";
+
+if (day === "Saturday" || day === "Sunday") {
+    console.log("It's a weekend!");
+} else {
+    console.log("It's a weekday.");
+}
+
+
+// Logical NOT (!):Example 1: Checking if a value is falsy:
+
+let loggedIn = true;
+
+if (!loggedIn) {
+    console.log("Please log in to continue.");
+} else {
+    console.log("You are already logged in.");
+}
+
+
