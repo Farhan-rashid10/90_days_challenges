cfe1dae3a81c740fbbf3e394683efa8a3db6ee8c
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-05 21:15:35 +0300

    adding some switches in js

---
+++ farhan repo/index.js
@@ -0,0 +1,90 @@
+let months = 1;
+let monthName;
+
+switch(months) {
+    case 1:
+        monthName = "January";
+    break;
+   
+    case 2 : 
+    monthName = "february";
+    
+    break;
+     
+    case 3 : 
+    monthName =  "march";
+    break;
+
+    case 4 : 
+    monthName =   "april";
+    break;
+
+    case 5 : 
+    monthName = "may";
+    break;
+
+    case 6 : 
+    monthName =  "june";
+    break;
+
+    case 7 : 
+    monthName = "july";
+    break;
+    default:
+
+}
+    console.log("this month is " + monthName);
+
+
+ //the student names of the class
+let names = 2;
+let userName;
+
+switch(names) {
+    case 1:
+        userName = "samira";
+    break;
+   
+    case 2 : 
+       userName = "katra";
+    
+    break;
+     
+    case 3 : 
+       userName =  "farhan";
+    break;
+
+    case 4 : 
+      userName=   "sumeya";
+    break;
+
+    case 5 : 
+     userName = "amina";
+    break;
+
+    case 6 : 
+    userName= "yussuf";
+    break;
+
+    case 7 : 
+    userName = "abubakar";
+    break;
+    default:
+
+}
+    console.log("my name is " + userName);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
