766ef5ba3f4865ff5f85d865214ea2b675a010ac
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-05 22:43:27 +0300

    javascript conditions

---
+++ moha repo/js-folder/conditions.js
@@ -0,0 +1,90 @@
+// conditions
+
+// let age = 16;
+// if (age >= 18){
+//     console.log('you can vote')
+// }
+// else{
+//     console.log('go home')
+// }
+
+// Define a variable
+ // The switch statement is used to perform different actions based on different conditions.
+// let age = 20
+// switch(age){
+
+// case  18:
+//     console.log('you can vote')
+//     break;
+//   case 17:
+//     console.log('you cant vote')
+//     break;
+//     default:
+//         console.log("none of the above")
+// }
+
+//let  grades = 10
+// switch(grades){
+//     case 80:
+//    case 70:
+//     case 90:
+//         case 60:
+//         console.log('you passed')
+//         break;
+//         case 40:
+//             case 50:
+//             case 30:
+//                 console.log('you are avarage')
+//                 break;
+//             default :
+//             console.log('you failed')
+// }
+
+
+// let months = 'june'
+// switch(months){
+//     case 'april':
+//         case 'august':
+//             case 'december':
+//                 console.log('warning wear heavy clothes')
+//                 break;
+//                 case 'july':
+//                     case 'june':
+//                         console.log('you can travel')
+//                         break;
+// }
+
+
+// let month = 'april';
+// if(month ==='april' || month === 'august' || month === 'december'){
+//     console.log('its raining dont go outside')
+// }else {
+//     console.log('you can travel')
+// }
+
+// //  week days example
+
+// let weekDays = 2
+// switch(weekDays){
+//     case 1:
+//         console.log('sunday')
+// break;
+// case 2:
+//     console.log('monday')
+//     break;
+// case 3 :
+//     console.log('tuesday')
+//     break;
+// }
+
+let age = 18
+switch(true){
+    case age >= 18:
+        console.log('you can vote')
+        break;
+        case age < 18:
+            console.log('go home')
+            break;
+}
+
+
