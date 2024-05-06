132050185889ab705c484dc234a701d64d9ed9c3
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-06 22:21:20 +0300

    creating a boolean file

---
+++ moha repo/js-folder/boolean.js
@@ -0,0 +1,31 @@
+//  A boolean data type can have only two values: true or false
+
+// Using Booleans in Conditional Statements:
+
+let isSunny = true;
+
+if (isSunny) {
+    console.log("It's sunny outside!");
+} else {
+    console.log("It's not sunny outside.");
+}
+
+
+//   Comparisons
+
+let num1 = 10;
+let num2 = 5;
+
+let isGreater = num1 > num2;
+console.log(isGreater); // Output: true
+
+
+// Logical Operators
+
+let hasMoney = true;
+let isHungry = false;
+
+let canBuyFood = hasMoney && isHungry;
+console.log(canBuyFood); // Output: false
+
+// 
