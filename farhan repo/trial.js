94272998173259a8006b985f5731338971160aa0
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-09 21:52:30 +0300

    trial js function

---
+++ farhan repo/trial.js
@@ -0,0 +1,14 @@
+// function addSum(a,b){
+//     console.log(a+b)
+// }
+//addSum(7,9)
+
+// function addSum(x,y){
+//     console.log(x+y)
+// }
+// addSum(30,10)
+
+function addSum(x,y){
+    console.log(x+y)
+}
+addSum(15,10)
