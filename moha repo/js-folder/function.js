e244c99b353230054e2b8948d075df4067fce409
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-09 23:35:29 +0300

    doing js function

---
+++ moha repo/js-folder/function.js
@@ -0,0 +1,57 @@
+// function exerciseByronThePoodle() {
+//     console.log("Wake Byron the poodle");
+//     console.log("Leash Byron the poodle");
+//     console.log("Walk to the park Byron the poodle");
+//     console.log("Throw the frisbee for Byron the poodle");
+//     console.log("Walk home with Byron the poodle");
+//     console.log("Unleash Byron the poodle");
+//   }
+
+
+// function intro 
+
+//   exerciseByronThePoodle()
+
+// function sayHelloToGuadalupe() {
+//   console.log("Hello, Guadalupe!");
+// }
+
+// sayHelloToGuadalupe()
+
+//  storing function inside a variable
+// here we call the variable not the function as always
+
+
+const doMath = function product(x, y) {
+  return( x + y)
+}
+  console.log(doMath(10,8))
+
+  function mathDo(a, b) {
+    return(a + b)
+  }
+
+  // Example 1: Function without arguments and return value
+function sayHello() {
+  console.log("Hello!");
+}
+
+sayHello(); // Output: Hello!
+
+// if immeadiately invoked function
+  
+function product (x, y) {
+  return (x * y);
+}
+  console.log(product(30, 40))
+
+  //  function expression its inside of variable
+
+  const fn = function addsum(a, b){
+    return (a+ b)
+  }
+  console.log(fn(100, 200))
+
+
+// function encapsulation
+
